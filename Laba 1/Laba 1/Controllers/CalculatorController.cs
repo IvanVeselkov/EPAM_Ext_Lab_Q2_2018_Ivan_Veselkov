f5d9ae5bbc23aa0bd49a7ce2d9095570cd64bba9
BenDunn <nerorus97@gmail.com>
2018-06-21 18:45:53 +0400

    commit 1

---
+++ Laba 1/Laba 1/Controllers/CalculatorController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Laba_1.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public string story;
+        // GET: Calculator
+        public ActionResult Calc(string arg1, string arg2, string math)
+        {
+           
+            Laba_1.Models.TResulte vdata = new Models.TResulte()
+            {
+                arg1 = arg1,
+                arg2 = arg2,
+                math = math,
+            };
+
+            return View(vdata);
+        }
+
+        public ActionResult Result(string arg1,string arg2,string math)
+        {
+            Laba_1.Models.TResulte vdata = new Models.TResulte()
+            {
+                arg1 = arg1,
+                arg2 = arg2,
+                math = math,
+            };
+
+            decimal a = 0;
+            decimal b = 0;
+            string result = "";
+            if (!decimal.TryParse(arg1, out a) || !decimal.TryParse(arg2, out b))
+                result = "Incorrect arguments";
+
+
+            switch (math)
+            {
+                case "+":
+                    result = (a + b).ToString();
+                    break;
+                case "-":
+                    result = (a - b).ToString();
+                    break;
+                case "*":
+                    result = (a * b).ToString();
+                    break;
+                case "/":
+                    if (b != 0)
+                        result = (a / b).ToString();
+                    else
+                        result = "exception division by zero";
+                    break;
+            }
+            DateTime loctime = DateTime.Now;
+            vdata.time = loctime.ToString();
+            
+            vdata.result = result;
+            story = vdata.story + "\n" + vdata.time + "   " + vdata.arg1 + vdata.math + vdata.arg2 + "=" + vdata.result;
+            vdata.story = story;
+            return View("Calc",vdata);
+        }//*/
+
+    }
+}
---
+++ Laba 1/Laba 1/Models/TResulte.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laba_1.Models
+{
+    public class TResulte
+    {
+        public string arg1 { get; set; }
+        public string arg2 { get; set; }
+
+        public string math { get; set; }
+        public string result { get; set; }
+        public string story { get; set; }
+       
+        public string time { get; set; }
+    }
+}
