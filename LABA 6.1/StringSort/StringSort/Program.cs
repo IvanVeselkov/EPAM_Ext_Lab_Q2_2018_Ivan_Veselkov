88e158eb4c1aa7a08ad927e7f87c7eba94da8678
IvanVeselkov <32011610+IvanVeselkov@users.noreply.github.com>
2018-07-23 14:22:22 +0400

    LABA 6.1 SortString commit 1

---
+++ LABA 6.1/StringSort/StringSort/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Globalization;
+
+namespace StringSort
+{
+
+    class Program
+    {
+        protected static bool needToReOrder(string s1, string s2)
+        {
+            for (int i = 0; i < (s1.Length > s2.Length ? s2.Length : s1.Length); i++)
+            {
+                if (s1.ToCharArray()[i] < s2.ToCharArray()[i]) return false;
+                if (s1.ToCharArray()[i] > s2.ToCharArray()[i]) return true;
+            }
+            return false;
+        }
+
+        delegate void Funtion(string[] a);
+
+        static void SortByLength(string[] arrStr)
+        {
+            for(int j=0;j<arrStr.Length;j++)
+            {
+                for (int i = 0; i < arrStr.Length - 1; i++)
+                {
+                    if(arrStr[i].Length==arrStr[i+1].Length)
+                    {
+                        if (needToReOrder(arrStr[i], arrStr[i + 1]))
+                        {
+                            string s = arrStr[i];
+                            arrStr[i] = arrStr[i + 1];
+                            arrStr[ + 1] = s;
+                        }
+                    }
+                    if (arrStr[i].Length > arrStr[i+1].Length)
+                    {
+                        string str = arrStr[i];
+                        arrStr[i] = arrStr[i + 1];
+                        arrStr[i + 1] = str;
+                    }
+                }
+            }
+           
+        }
+
+        static void Main(string[] args)
+        {
+            string[] arrStr = new string[3];
+             for(int i=0;i<10;i++)
+             {
+                 arrStr[i] = Console.ReadLine();
+             }
+
+           
+
+            Funtion funk1 = new Funtion(SortByLength);
+
+            funk1(arrStr);
+                   
+            foreach(string s in arrStr)
+            {
+                Console.WriteLine(s);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
